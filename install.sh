d383980a768997fe83d0bb99279cc734deb1f4cb
Louis Livingston <livingston.louis@gmail.com>
2020-05-02 16:14:25 -0400

    autostart

--- install.sh
+++ install.sh
@@ -18,7 +18,10 @@ RVCGITBRANCH="master"
 RVC="$HOME/retropie-volume-control"
 RP="$HOME/RetroPie"
 RPMENU="$RP/retropiemenu"
+RPCONFIGS="/opt/retropie/configs/all"
 THCONFIGS="/etc/triggerhappy/triggers.d"
+
+SCRIPTPATH=$(realpath $0)
 
 ########################
 ##  Remove Old Files  ##
@@ -27,6 +30,7 @@ echo -e " ${LRED}-${NC}${WHITE} Removing old files...${NC}"
 rm -rf $RVC
 sudo rm "$THCONFIGS/sound.conf"
 rm "$RPMENU/sound-config.sh"
+rm "$RPMENU/retropie_volume_control.sh"
 
 #############################
 ##Packages and Dependencies##
@@ -37,7 +41,7 @@ sleep 1
 echo -e " ${LRED}-${NC}${WHITE} Checking packages and dependencies...${NC}"
 sleep 1
 
-packages=("dialog" "python3-dev" "python-dev" "python3-pip" "python-pip" "libasound2-dev" "python3-dialog" "unzip")
+packages=("dialog" "python3-dev" "triggerhappy" "python3-pip" "libasound2-dev" "python3-dialog" "unzip")
 
 for package in "${packages[@]}"; do
 	if dpkg -s $package >/dev/null 2>&1; then
@@ -89,7 +93,7 @@ echo -e " ${LRED}--${NC}${WHITE} Writing system files...${NC}${ORANGE}\n"
 sleep 1
 
 cd $RPMENU
-sudo cp $RVC/sound-config.sh .
+sudo cp $RVC/retropie_volume_control.sh .
 
 cd $THCONFIGS
 RVCFILES=("sound.conf")
@@ -104,13 +108,46 @@ sleep 1
 ##########################
 echo -e "\n ${LRED}-${NC}${WHITE} Setting permissions...${NC}\n"
 cd $RPMENU
-chmod +x sound-config.sh
+chmod +x retropie_volume_control.sh
 cd $RVC
-chmod +x sound-config.sh
+chmod +x retropie_volume_control.sh
 sleep 1
 
 ########################
 ########################
+
+cd $RPCONFIGS
+echo -e " ${LRED}--${NC}${WHITE} Writing on runcommand commands...${NC}"
+sleep 1
+function runcommandsetup(){
+
+	file=$1
+	command=$2
+
+	if [ ! -e $file ]; then
+			echo -e " ${LRED}---${NC}${WHITE} $file not found, creating...${NC}"
+			sleep 1
+			touch $file
+			sleep 0.5
+			chmod a+rwx $file
+			sleep 0.5
+			echo "$command" > $file
+		else
+			echo -e " ${LRED}---${NC}${WHITE} $file found, writing...${NC}"
+			sleep 1
+			#use sudo because, owner can be root or file created incorrectly for any reason
+			sudo chmod 777 $file
+			sleep 0.5
+			sed -i "/rvc_system.sh/d" $file
+			[ -s $file ] && sed -i "1i $command" $file || echo "$command" > $file
+	fi
+}
+runcommandsetup "runcommand-onstart.sh" "bash \$HOME/retropie-volume-control/rvc_system.sh -i"
+#use sudo because, owner can be root or file created incorrectly for any reason
+sudo chmod 777 autostart.sh
+sed -i "/bgm_system.sh/d" autostart.sh
+sed -i "1 i bash \$HOME/RetroPie-BGM-Player/bgm_system.sh -i --autostart" autostart.sh
+sleep 1
 
 ##########################
 ## Restart Triggerhappy ##
@@ -127,3 +164,14 @@ sleep 1
 ###############
 echo -e "\n ${LRED}-${NC}${WHITE} Installation Complete!...${NC}\n"
 sleep 1
+
+########################
+##       Restart      ##
+########################
+echo -e " ${LRED}[${NC}${LGREEN} Restart System ${NC}${LRED}]${NC}"
+echo -e " ${LRED}-${NC}${WHITE} To finish, we need to reboot.${NC}${ORANGE}\n"
+read -n 1 -s -r -p " Press any key to Restart."
+echo -e "${NC}\n"
+(rm -f $SCRIPTPATH; sudo reboot)
+########################
+########################
